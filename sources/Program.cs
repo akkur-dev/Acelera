d2ebb2384ccc241654d49a829e9207ae55781a20
Alexander <akkur-software@gmail.com>
2026-08-30 20:46:24 +0600

    Initial commit

---
+++ sources/Program.cs
@@ -0,0 +1,16 @@
+namespace Acelera.App;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        builder.Services.AddAuthorization();
+
+        var app = builder.Build();
+
+        app.UseHttpsRedirection();
+        app.UseAuthorization();
+        app.Run();
+    }
+}
